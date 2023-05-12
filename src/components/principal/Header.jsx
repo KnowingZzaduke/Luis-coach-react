cc51014521e6c685abb89092ca4c85c0c751e5b0
KnowinZzaduke <josex.developer@gmail.com>
2023-05-11 22:36:49 -0500

    Estructurando las secciones

--- src/components/principal/Header.jsx
+++ src/components/principal/Header.jsx
@@ -1,3 +1,4 @@
+import videoLogoLuis from "/video/L-video-fondo.mp4";
 import LogoLuis from "/img/luis-transparent.png";
 import {
   FaBars,
@@ -8,6 +9,7 @@ import {
   FaPhoneAlt,
 } from "react-icons/fa";
 import { useState } from "react";
+import { motion } from "framer-motion";
 export function Header() {
   const [menu, setMenu] = useState(false);
   function openMenu() {
@@ -53,17 +55,31 @@ export function Header() {
           <FaBars onClick={openMenu} />
         </div>
       </nav>
-      <div className="info">
+      <div className="content_info">
         <div className="left">
           <h1>Luis Buelvas Conferencista & Coach Financiero </h1>
+          <div className="content_start">
+            <h3>
+              Está página ofrece una gran variedad de cursos que orientan a las
+              personas como tener libertad financiera y como lograr éxito.{" "}
+            </h3>
+            <div className="content_button"></div>
+          </div>
         </div>
-        <div className="content_start">
-          <h3>
-            Está página ofrece una gran variedad de cursos que orientan a las
-            personas como tener libertad financiera y como lograr éxito.{" "}
-          </h3>
-          <div className="content_button"></div>
-        </div>
+        <motion.div
+          className="right"
+          initial={{ opacity: 0, scale: 0.5 }}
+          animate={{ opacity: 1, scale: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <video
+            src={videoLogoLuis}
+            className="video_found"
+            autoPlay
+            loop
+            muted
+          />
+        </motion.div>
       </div>
     </header>
   );
