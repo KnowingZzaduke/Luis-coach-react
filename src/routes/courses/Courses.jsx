c9760c82b96c9690583a5eaa8dade21676dbde95
KnowinZzaduke <josex.developer@gmail.com>
2023-05-26 16:59:11 -0500

    Agregando detalles en la página del contenido de los cursos

--- src/routes/courses/Courses.jsx
+++ src/routes/courses/Courses.jsx
@@ -1,10 +1,12 @@
 import { NavbarC } from "../../components/courses/Navbar";
 import { Outlet } from "react-router-dom";
+import { Footer } from "../../components/principal/Footer";
 export function Courses() {
   return (
     <div className="content_material-courses">
       <NavbarC />
       <Outlet />
+      <Footer />
     </div>
   );
 }
